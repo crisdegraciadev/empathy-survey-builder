bce90e53f3908c7e2ce214598562e05e9aae86dc
crisdegraciadev <crisdegraciadev@gmail.com>
2024-05-19 20:38:56 +0200

    test: config jest custom paths

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'jest-preset-angular',
+  roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleNameMapper: {
+    '@ui/(.*)': '<rootDir>/src/app/ui/$1',
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@features/(.*)': '<rootDir>/src/app/features/$1',
+    '@layout/(.*)': '<rootDir>/src/app/features/$1',
+  },
 };
